b5256538ab33e342c0a982255607fd8215cd5aba
itsaryanchauhan <146752682+itsaryanchauhan@users.noreply.github.com>
2024-05-19 01:33:34 +0530

    Update project.py

--- project.py
+++ project.py
@@ -6,16 +6,16 @@ def generate_story_ideas(model_name):
     while True:
         user_input = input("Enter a story prompt (e.g., A young inventor stumbles upon a magical device): ")
         
+        if user_input.lower() == 'end':
+            print("Exiting ..")
+            break
+
         output = model.prompt_main(user_input,
                                   prompt_name="generate_story",
                                   temperature=0.40)
         story = output["llm_response"].strip("\n")
 
         print("Story: ", story)
-        
-        if user_input.lower() == 'end':
-            print("Exiting chatbot...")
-            break
 
 if __name__ == "__main__":
     model_name = "phi-3-gguf"

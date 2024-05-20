fcdae2d081b8c1fa3cd76e0cdef1e176f576ed0d
itsaryanchauhan <146752682+itsaryanchauhan@users.noreply.github.com>
2024-05-20 13:06:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request, jsonify
 from story_generator import generate_story_ideas
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')  # Add this line
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/generate_story', methods=['POST'])
 def generate_story():
-    data = request.get_json()
-    genre = data['genre']
-    character = data['character']
-    setting = data['setting']
-    conflict = data['conflict']
+  data = request.get_json()
+  genre = data['genre']
+  character = data['character']
+  setting = data['setting']
+  conflict = data['conflict']
 
-    generated_story = generate_story_ideas(model_name="phi-3-gguf", genre=genre, character=character, setting=setting, conflict=conflict)
+  generated_story = generate_story_ideas(model_name="phi-3-gguf", genre=genre, character=character, setting=setting, conflict=conflict)
 
-    return jsonify({'generated_story': generated_story})
+  return jsonify({'generated_story': generated_story})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
